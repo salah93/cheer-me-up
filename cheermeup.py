4d2a72ae7898a25986d3d5699db407d0e10129c0
Salah Ahmed <salah.ahmed.hslc@gmail.com>
2016-07-16 18:11:28 -0400

    implement add argument

--- cheermeup.py
+++ cheermeup.py
@@ -1,13 +1,13 @@
 #! /usr/local/bin/python3.4
 import random
+import sqlite3
 import webbrowser
 from argparse import ArgumentParser
-from os import environ
-import sqlite3
 
 
 conn = sqlite3.connect('funny.db')
 c = conn.cursor()
+
 
 def get_funny():
     videos = c.execute("Select * from videos").fetchall()
@@ -15,19 +15,18 @@ def get_funny():
 
 
 def add(title, url):
-   c.execute("INSERT INTO videos values(?,?)", (title, url))
+    c.execute("INSERT INTO videos values(?,?)", (title, url))
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     group = parser.add_subparsers(dest='add')
-    # get = group.add_parser('get')
     add_command = group.add_parser('add')
-    add_command.add_argument('-t')
-    add_command.add_argument('-u')
+    add_command.add_argument('-t', required=True)
+    add_command.add_argument('-u', required=True)
     args = parser.parse_args()
     if args.add:
-        import IPython; IPython.embed()
+        add(args.t, args.u)
     else:
         cheerup = get_funny()
         print(cheerup[0])
